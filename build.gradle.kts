91365b8ffedefe92d4f264446466999c92f07aee
lingting <sunlisten.gzm@gmail.com>
2024-01-25 14:02:25 +0800

    :wrench: 优化依赖配置

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,6 @@
 var javaProjects = subprojects.filter { it.name.startsWith("lingting-") }
+var javaVersion = JavaVersion.VERSION_17
+var encoding = "UTF-8"
 
 allprojects {
     group = "live.lingting.framework"
@@ -22,19 +24,36 @@ allprojects {
 
 configure(javaProjects) {
     print(project.displayName)
-
     apply {
         plugin("java")
     }
 
     dependencies {
-        add("testImplementation", platform("org.junit:junit-bom:5.9.1"))
+        val springBootVersion = "3.2.1";
+        val mapstructVersion = "1.5.3.Final"
+        val lombokVersion = "1.18.24"
+        val lombokMapstructBindingVersion = "0.2.0"
+        val awaitilityVersion = "4.2.0"
+        val junitVersion = "5.9.1"
+
+        add("implementation", platform("org.springframework.boot:spring-boot-dependencies:${springBootVersion}"))
+        add("implementation", "org.slf4j:slf4j-api")
+
+        add("compileOnly", "org.mapstruct:mapstruct:${mapstructVersion}")
+        add("compileOnly", "org.projectlombok:lombok:${lombokVersion}")
+
+        add("annotationProcessor", "org.projectlombok:lombok:${lombokVersion}")
+        add("annotationProcessor", "org.mapstruct:mapstruct-processor:${mapstructVersion}")
+        add("annotationProcessor", "org.projectlombok:lombok-mapstruct-binding:${lombokMapstructBindingVersion}")
+
+        add("testImplementation", "org.awaitility:awaitility:${awaitilityVersion}")
+        add("testImplementation", platform("org.junit:junit-bom:${junitVersion}"))
         add("testImplementation", "org.junit.jupiter:junit-jupiter")
     }
 
     configure<JavaPluginExtension> {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
     }
 
     tasks.withType<Test> {
@@ -42,7 +61,7 @@ configure(javaProjects) {
     }
 
     tasks.withType<JavaCompile> {
-        options.encoding = "UTF-8"
+        options.encoding = encoding
     }
 }
 
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,38 @@
+pluginManagement {
+    repositories {
+        mavenLocal()
+
+        maven(url = "https://mirrors.huaweicloud.com/repository/maven/")
+        maven(url = "https://maven.aliyun.com/repository/public/")
+        maven(url = "https://maven.aliyun.com/repository/spring")
+
+        mavenCentral()
+        google()
+    }
+}
+
+dependencyResolutionManagement {
+    // 编写版本目录的依赖库
+    versionCatalogs {
+        create("libs") {
+//            val slf4jVersion = "1.7.36"
+//            val mapstructVersion = "1.5.3.Final"
+//            val lombokVersion = "1.18.24"
+//            val lombokMapstructBindingVersion = "0.2.0"
+//
+//            library("slf4j", "org.slf4j", "slf4j-api").version(slf4jVersion)
+//            library("mapstruct", "org.mapstruct", "mapstruct").version(mapstructVersion)
+//            library("mapstruct-processor", "org.mapstruct", "mapstruct-processor").version(mapstructVersion)
+//            library("lombok", "org.projectlombok", "lombok").version(lombokVersion)
+//            library("lombok_mapstruct", "org.projectlombok", "lombok-mapstruct-binding").version(lombokMapstructBindingVersion)
+//
+//            bundle("globalCompileOnly", listOf("mapstruct", "lombok"))
+//            bundle("annotationProcessor", listOf("mapstruct-processor", "lombok", "lombok_mapstruct"))
+        }
+    }
+
+}
+
+
 rootProject.name = "lingting-framework"
 include("lingting-core")
