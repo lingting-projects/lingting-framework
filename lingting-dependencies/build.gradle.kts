c486ca9199705f541fcf61c7f9596d0bd4735206
lingting <sunlisten.gzm@gmail.com>
2024-01-31 17:31:01 +0800

    :wrench: 支持发布到maven

--- lingting-dependencies/build.gradle.kts
+++ lingting-dependencies/build.gradle.kts
@@ -1,25 +1,37 @@
-plugins {
-    id("java-platform")
+val bomList = mutableListOf<Provider<MinimalExternalModuleDependency>>()
+val apiList = mutableListOf<Provider<MinimalExternalModuleDependency>>()
+
+// 三方依赖解析
+for (catalog in versionCatalogs) {
+    for (alias in catalog.libraryAliases) {
+        val optional = catalog.findLibrary(alias)
+        if (optional.isEmpty) {
+            continue
+        }
+        val provider = optional.get()
+        if (alias.contains("Dependencies")) {
+            bomList.add(provider)
+        } else {
+            apiList.add(provider)
+        }
+    }
+}
+
+javaPlatform {
+    allowDependencies()
 }
 
 dependencies {
+
+    bomList.forEach {
+        api(platform(it))
+    }
+
     constraints {
         // 三方依赖约束
-        for (catalog in versionCatalogs) {
-            for (alias in catalog.libraryAliases) {
-                val optional = catalog.findLibrary(alias)
-                if (optional.isEmpty) {
-                    continue
-                }
-                val provider = optional.get()
-                if (alias.contains("Dependencies")) {
-                    api(platform(provider))
-                } else {
-                    api(provider)
-                }
-            }
+        apiList.forEach {
+            api(it)
         }
-
         // 子项目依赖约束
         project.parent?.subprojects?.forEach {
             if (it != project) {
