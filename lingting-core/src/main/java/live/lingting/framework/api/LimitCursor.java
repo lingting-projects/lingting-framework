5d5ae3315686632883c9ba2c433a11820b1f0628
lingting <sunlisten.gzm@gmail.com>
2024-03-08 16:28:22 +0800

    :sparkles: 添加分页和滚动查询的游标支持

---
+++ lingting-core/src/main/java/live/lingting/framework/api/LimitCursor.java
@@ -0,0 +1,31 @@
+package live.lingting.framework.api;
+
+import live.lingting.framework.function.ThrowingFunction;
+import live.lingting.framework.value.CursorValue;
+import lombok.SneakyThrows;
+
+import java.util.List;
+
+/**
+ * @author lingting 2023-12-29 11:32
+ */
+public class LimitCursor<T> extends CursorValue<T> {
+
+	private final ThrowingFunction<Long, PaginationResult<T>> limit;
+
+	private long index;
+
+	public LimitCursor(ThrowingFunction<Long, PaginationResult<T>> limit) {
+		this.limit = limit;
+		this.index = 1;
+	}
+
+	@SneakyThrows
+	@Override
+	protected List<T> nextBatchData() {
+		PaginationResult<T> result = limit.apply(index);
+		index++;
+		return result.getRecords();
+	}
+
+}
---
+++ lingting-core/src/main/java/live/lingting/framework/api/ScrollCursor.java
@@ -0,0 +1,39 @@
+package live.lingting.framework.api;
+
+import live.lingting.framework.function.ThrowingFunction;
+import live.lingting.framework.util.CollectionUtils;
+import live.lingting.framework.value.CursorValue;
+import lombok.SneakyThrows;
+
+import java.util.List;
+
+/**
+ * @author lingting 2023-12-29 11:32
+ */
+public class ScrollCursor<T, S> extends CursorValue<T> {
+
+	private final ThrowingFunction<S, ScrollResult<T, S>> scroll;
+
+	private S scrollId;
+
+	public ScrollCursor(ThrowingFunction<S, ScrollResult<T, S>> scroll, S scrollId, List<T> data) {
+		this.scroll = scroll;
+		this.scrollId = scrollId;
+		// 初始数据就为空, 直接结束
+		if (CollectionUtils.isEmpty(data)) {
+			empty = true;
+		}
+		else {
+			current.addAll(data);
+		}
+	}
+
+	@SneakyThrows
+	@Override
+	protected List<T> nextBatchData() {
+		ScrollResult<T, S> result = scroll.apply(scrollId);
+		scrollId = result.getCursor();
+		return result.getRecords();
+	}
+
+}
