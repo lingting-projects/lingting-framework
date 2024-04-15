04a2f6f014912dd23d998dda29eb12c087ab4f2c
lingting <sunlisten.gzm@foxmail.com>
2024-04-15 15:31:18 +0800

    :zap: scope 属性提供更多获取方法

--- lingting-security/src/main/java/live/lingting/framework/security/domain/SecurityScope.java
+++ lingting-security/src/main/java/live/lingting/framework/security/domain/SecurityScope.java
@@ -5,7 +5,10 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 /**
  * @author lingting 2023-03-29 20:25
@@ -58,8 +61,31 @@ public class SecurityScope {
 	 * 此scope是否为已登录用户
 	 */
 	public boolean isLogin() {
-		return StringUtils.hasText(getToken()) && StringUtils.hasText(getUserId()) && getIsSystem() != null
-				&& getIsEnabled() != null;
+		boolean tokenAvailable = StringUtils.hasText(getToken());
+		boolean userAvailable = StringUtils.hasText(getUserId());
+		boolean enableAvailable = getIsEnabled() != null;
+		return tokenAvailable && userAvailable && getIsSystem() != null && enableAvailable;
+	}
+
+	public Object attribute(String key) {return attributes == null || attributes.isEmpty() ? null : attributes.get(key);}
+
+	public <T> T attribute(String key, Function<Optional<Object>, T> func) {
+		return func.apply(Optional.ofNullable(attribute(key)));
+	}
+
+	public <T> T attribute(String key, T defaultValue, Function<Optional<Object>, T> func) {
+		return attribute(key, defaultValue, Optional::isEmpty, func);
+	}
+
+	/**
+	 * @param usingDefault 如果返回true表示使用默认值
+	 */
+	public <T> T attribute(String key, T defaultValue, Predicate<Optional<Object>> usingDefault, Function<Optional<Object>, T> func) {
+		Optional<Object> optional = Optional.ofNullable(attribute(key));
+		if (usingDefault.test(optional)) {
+			return defaultValue;
+		}
+		return func.apply(optional);
 	}
 
 }
