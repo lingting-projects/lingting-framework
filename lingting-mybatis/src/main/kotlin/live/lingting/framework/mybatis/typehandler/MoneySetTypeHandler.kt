cb1de3b9e7630b7c400fb6501c05b0236a4685db
lingting <sunlisten.gzm@foxmail.com>
2024-12-06 13:57:20 +0800

    :bug: 修复 AbstractJacksonTypeHandler 部分情况NLP

--- lingting-mybatis/src/main/kotlin/live/lingting/framework/mybatis/typehandler/MoneySetTypeHandler.kt
+++ lingting-mybatis/src/main/kotlin/live/lingting/framework/mybatis/typehandler/MoneySetTypeHandler.kt
@@ -3,7 +3,7 @@ package live.lingting.framework.mybatis.typehandler
 import com.fasterxml.jackson.core.type.TypeReference
 import live.lingting.framework.money.Money
 
-class MoneySetTypeHandler : AbstractSetTypeHandler<Money>() {
+open class MoneySetTypeHandler : AbstractSetTypeHandler<Money>() {
 
     override val reference = object : TypeReference<Set<Money>>() {
     }
