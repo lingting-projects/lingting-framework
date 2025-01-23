3f5486612e9b17575857ada341481b892454cfe6
lingting <sunlisten.gzm@foxmail.com>
2025-01-23 14:16:44 +0800

    :white_check_mark: 线程池测试添加虚拟线程

--- lingting-core/src/test/kotlin/live/lingting/framework/thread/ThreadPoolTest.kt
+++ lingting-core/src/test/kotlin/live/lingting/framework/thread/ThreadPoolTest.kt
@@ -1,13 +1,9 @@
 package live.lingting.framework.thread
 
 import java.time.Duration
-import java.util.concurrent.LinkedBlockingQueue
-import java.util.concurrent.ThreadPoolExecutor
-import java.util.concurrent.ThreadPoolExecutor.CallerRunsPolicy
-import java.util.concurrent.TimeUnit
-import java.util.concurrent.atomic.AtomicBoolean
-import live.lingting.framework.concurrent.Await
+import kotlin.test.assertNotEquals
 import live.lingting.framework.util.MdcUtils
+import live.lingting.framework.util.ThreadUtils
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
@@ -16,32 +12,38 @@ import org.junit.jupiter.api.Test
  */
 class ThreadPoolTest {
     @Test
+    fun testPool() {
+        ThreadUtils.instance = ThreadPool.instance
+        testMdc()
+    }
+
+    @Test
+    fun testVirtual() {
+        ThreadUtils.instance = VirtualThread.instance
+        testMdc()
+    }
+
     fun testMdc() {
-        val atomic = AtomicBoolean(false)
+        val instance = ThreadUtils.instance
+        val async = Async(instance.executor())
         val traceId = MdcUtils.setTraceId()
-
         val currentName = Thread.currentThread().name
-        val executor = ThreadPoolExecutor(
-            1, 1, 1, TimeUnit.MINUTES, LinkedBlockingQueue(1),
-            CallerRunsPolicy()
-        )
-        ThreadPool.update(executor)
 
-        ThreadPool.execute {
+        async.submit {
             Thread.sleep(Duration.ofSeconds(1).toMillis())
             assertEquals(traceId, MdcUtils.traceId)
-            atomic.set(true)
         }
-        ThreadPool.execute {
+
+        async.submit {
             Thread.sleep(Duration.ofSeconds(1).toMillis())
             assertEquals(traceId, MdcUtils.traceId)
-            atomic.set(true)
         }
-        ThreadPool.execute {
-            assertEquals(currentName, Thread.currentThread().name)
+        async.submit {
+            assertNotEquals(currentName, Thread.currentThread().name)
             assertEquals(traceId, MdcUtils.traceId)
         }
         assertEquals(traceId, MdcUtils.traceId)
-        Await.waitTrue { atomic.get() }
+        async.await()
     }
+
 }
