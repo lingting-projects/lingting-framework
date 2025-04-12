0f7511cba68fccd1524e4e3089905eb41c396ae8
lingting <sunlisten.gzm@foxmail.com>
2025-04-12 16:37:46 +0800

    :zap: 添加map扁平化支持

--- lingting-core/src/main/kotlin/live/lingting/framework/util/CollectionUtils.kt
+++ lingting-core/src/main/kotlin/live/lingting/framework/util/CollectionUtils.kt
@@ -1,6 +1,6 @@
 package live.lingting.framework.util
 
-import java.util.Collections
+import java.util.*
 
 /**
  * @author lingting
@@ -123,5 +123,34 @@ object CollectionUtils {
         }
         return map
     }
+
+    private fun flattenMap(value: Any?, key: String = ""): Map<String, Any> {
+        val result = HashMap<String, Any>()
+        when {
+            value is Map<*, *> -> {
+                value.forEach { k, v ->
+                    val newKey = if (key.isBlank()) "$k" else "$key.$k"
+                    val flatten = flattenMap(v, newKey)
+                    result.putAll(flatten)
+                }
+            }
+
+            value is Collection<*> -> {
+                value.forEachIndexed { i, v ->
+                    val newKey = "$key[$i]"
+                    val flatten = flattenMap(v, newKey)
+                    result.putAll(flatten)
+                }
+            }
+
+            value != null -> {
+                result.put(key, value)
+            }
+        }
+        return result
+    }
+
+    @JvmStatic
+    fun Map<*, *>.flatten(): Map<String, Any> = flattenMap(this)
 
 }
