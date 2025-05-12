431dce417dfe737e921e9673b4340fdcf263d4ad
lingting <sunlisten.gzm@foxmail.com>
2025-05-12 11:52:17 +0800

    :zap: 默认线程池不使用State包裹, 由具体的实现决定

--- lingting-core/src/main/kotlin/live/lingting/framework/thread/platform/PlatformThread.kt
+++ lingting-core/src/main/kotlin/live/lingting/framework/thread/platform/PlatformThread.kt
@@ -1,7 +1,7 @@
 package live.lingting.framework.thread.platform
 
-import live.lingting.framework.thread.executor.PolicyThreadPoolExecutor
-import live.lingting.framework.thread.executor.PolicyThreadPoolProperties
+import live.lingting.framework.thread.executor.DelegationExecutorService
+import live.lingting.framework.thread.executor.PolicyExecutorService
 import live.lingting.framework.thread.executor.StateKeepExecutorService
 import live.lingting.framework.thread.platform.PlatformThread.newExecutor
 import java.util.concurrent.ThreadFactory
@@ -10,7 +10,7 @@ import java.util.concurrent.ThreadPoolExecutor
 /**
  * @author lingting 2025/5/12 10:20
  */
-object PlatformThread : StateKeepExecutorService(newExecutor()) {
+object PlatformThread : DelegationExecutorService(PolicyExecutorService(StateKeepExecutorService(newExecutor()))) {
 
     @JvmStatic
     fun newExecutor(): ThreadPoolExecutor {
@@ -19,8 +19,8 @@ object PlatformThread : StateKeepExecutorService(newExecutor()) {
 
     @JvmStatic
     fun newExecutor(factory: ThreadFactory): ThreadPoolExecutor {
-        val properties = PolicyThreadPoolProperties.create(factory)
-        return PolicyThreadPoolExecutor(properties)
+        val properties = ThreadPoolProperties.byDefault(factory)
+        return properties.build()
     }
 
 }
--- lingting-core/src/main/kotlin/live/lingting/framework/thread/virtual/VirtualThread.kt
+++ lingting-core/src/main/kotlin/live/lingting/framework/thread/virtual/VirtualThread.kt
@@ -1,11 +1,12 @@
 package live.lingting.framework.thread.virtual
 
-import live.lingting.framework.thread.executor.StateKeepExecutorService
+import live.lingting.framework.thread.executor.DelegationExecutorService
+import live.lingting.framework.thread.executor.PolicyExecutorService
 
 /**
  * @author lingting 2025/5/12 10:10
  */
-object VirtualThread : StateKeepExecutorService(VirtualThreadExecutorService()) {
+object VirtualThread : DelegationExecutorService(PolicyExecutorService(VirtualThreadExecutorService())) {
 
     @JvmField
     val isSupport: Boolean = VirtualThreadExecutorService.isSupport
