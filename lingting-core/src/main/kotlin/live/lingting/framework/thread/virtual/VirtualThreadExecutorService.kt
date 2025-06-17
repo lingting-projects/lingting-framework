d667653df2277a7ac3ac1f452f7053ca061a3bb7
lingting <sunlisten.gzm@foxmail.com>
2025-06-17 15:00:06 +0800

    :bug: jdk8不支持虚拟线程

--- lingting-core/src/main/kotlin/live/lingting/framework/thread/virtual/VirtualThreadExecutorService.kt
+++ lingting-core/src/main/kotlin/live/lingting/framework/thread/virtual/VirtualThreadExecutorService.kt
@@ -1,9 +1,7 @@
 package live.lingting.framework.thread.virtual
 
-import live.lingting.framework.thread.executor.PerThreadExecutor
 import live.lingting.framework.thread.executor.StateKeepExecutorService
 import live.lingting.framework.thread.platform.PlatformThread
-import live.lingting.framework.util.ClassUtils
 
 /**
  * @author lingting 2025/5/12 10:11
@@ -13,17 +11,10 @@ class VirtualThreadExecutorService : StateKeepExecutorService {
     companion object {
 
         @JvmStatic
-        val isSupport = ClassUtils.method(Thread::class.java, "ofVirtual") != null
+        val isSupport = false
 
     }
 
-    constructor() : super(
-        if (isSupport) {
-            val factory = Thread.ofVirtual().name("vt-", 0).factory()
-            PerThreadExecutor(factory)
-        } else {
-            PlatformThread.delegator
-        }
-    )
+    constructor() : super(PlatformThread.delegator)
 
 }
