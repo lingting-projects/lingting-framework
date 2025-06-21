c2e5384ad5beabcee998db21197c46101b968114
lingting <sunlisten.gzm@foxmail.com>
2025-06-21 17:33:54 +0800

    :sparkles: mybatis自定义方法支持查询指定配置的数据库类型

--- lingting-mybatis/src/main/kotlin/live/lingting/framework/mybatis/methods/InsertIgnore.kt
+++ lingting-mybatis/src/main/kotlin/live/lingting/framework/mybatis/methods/InsertIgnore.kt
@@ -4,7 +4,16 @@ package live.lingting.framework.mybatis.methods
  * @author lingting 2020/5/27 11:47
  */
 class InsertIgnore : AbstractInsert("insertIgnore") {
+
     override fun getSql(): String {
-        return "<script>insert ignore into %s %s value %s</script>"
+        val dbType = dbType()
+        val str = when (dbType) {
+            "mysql" -> "INSERT IGNORE INTO %s %s VALUES %s"
+            "sqlite" -> "INSERT OR IGNORE INTO %s %s VALUES %s"
+            "postgresql" -> "INSERT INTO %s %s VALUES %s ON CONFLICT DO NOTHING"
+            else -> throw UnsupportedOperationException("Unsupported database type: $dbType")
+        }
+        return "<script>${str}</script>"
     }
+
 }
