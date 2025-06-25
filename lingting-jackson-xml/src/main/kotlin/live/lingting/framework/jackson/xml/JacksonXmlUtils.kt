12d705efa3ca9705d5edef81108b4e90d34fef55
lingting <sunlisten.gzm@foxmail.com>
2025-06-25 18:08:33 +0800

    :zap: 统一jackson默认配置

--- lingting-jackson-xml/src/main/kotlin/live/lingting/framework/jackson/xml/JacksonXmlUtils.kt
+++ lingting-jackson-xml/src/main/kotlin/live/lingting/framework/jackson/xml/JacksonXmlUtils.kt
@@ -1,21 +1,10 @@
 package live.lingting.framework.jackson.xml
 
-import com.fasterxml.jackson.core.json.JsonReadFeature
 import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.JavaType
 import com.fasterxml.jackson.databind.JsonNode
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
-import live.lingting.framework.jackson.module.BooleanModule
-import live.lingting.framework.jackson.module.DataSizeModule
-import live.lingting.framework.jackson.module.EnumModule
-import live.lingting.framework.jackson.module.JavaTimeModule
-import live.lingting.framework.jackson.module.MoneyModule
-import live.lingting.framework.jackson.module.RModule
-import live.lingting.framework.jackson.provider.NullSerializerProvider
-import live.lingting.framework.jackson.sensitive.SensitiveModule
+import live.lingting.framework.jackson.JacksonUtils
 import live.lingting.framework.jackson.wrapper.JacksonWrapper
 import java.lang.reflect.Type
 import java.util.function.Consumer
@@ -28,45 +17,13 @@ import kotlin.reflect.KClass
 object JacksonXmlUtils {
 
     @JvmStatic
-    var mapper: XmlMapper = defaultConfig(XmlMapper())
+    var mapper: XmlMapper = JacksonUtils.defaultConfig(XmlMapper())
         set(value) {
             field = value
             wrapper.mapper = value
         }
 
     val wrapper = JacksonWrapper(mapper)
-
-    @JvmStatic
-    fun <T : ObjectMapper> defaultConfig(mapper: T): T {
-        // 序列化时忽略未知属性
-        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-        // 单值元素可以被设置成 array, 防止处理 ["a"] 为 List<String> 时报错
-        mapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY)
-        // 有特殊需要转义字符, 不报错
-        mapper.enable(JsonReadFeature.ALLOW_UNESCAPED_CONTROL_CHARS.mappedFeature())
-
-        // 空对象不报错
-        mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
-
-        // 空值处理
-        mapper.setSerializerProvider(NullSerializerProvider())
-
-        // 布尔处理器
-        mapper.registerModule(BooleanModule())
-        // 时间解析器
-        mapper.registerModule(JavaTimeModule())
-        // 枚举解析器
-        mapper.registerModule(EnumModule())
-        // R 解析器
-        mapper.registerModule(RModule())
-        // 脱敏相关
-        mapper.registerModule(SensitiveModule())
-        // 金额相关
-        mapper.registerModule(MoneyModule())
-        // 数据大小
-        mapper.registerModule(DataSizeModule())
-        return mapper
-    }
 
     @JvmStatic
     fun config(consumer: Consumer<XmlMapper>) = wrapper.config(consumer)
